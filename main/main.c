59b4102c22153fd700f64e6f135c36247921d988
tesslaray <cristian.valdivia@alumnos.usm.cl>
2021-07-05 20:19:55 -0400

    feat: first commit

--- main/main.c
+++ main/main.c
@@ -7,7 +7,7 @@ void app_main(void)
 {
     int i = 0;
     while (1) {
-        printf("[%d] Hello world!\n", i);
+        printf("[%d] Hello iOLED!\n", i);
         i++;
         vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
