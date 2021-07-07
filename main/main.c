c0ddef50b0fd5ac0b02d06e0b832b17e15ff1633
tesslaray <cristian.valdivia@alumnos.usm.cl>
2021-07-07 00:18:34 -0400

    feat: change led to red and yellow

--- main/main.c
+++ main/main.c
@@ -23,7 +23,6 @@ static const char *TAG = "example";
 
 static uint8_t s_led_state = 0;
 
-#ifdef CONFIG_BLINK_LED_RMT
 static led_strip_t *pStrip_a;
 
 static void blink_led(void)
@@ -34,8 +33,8 @@ static void blink_led(void)
         pStrip_a->set_pixel(pStrip_a, 0, 16, 16, 16);
         pStrip_a->set_pixel(pStrip_a, 1, 16, 16, 16);
         pStrip_a->set_pixel(pStrip_a, 2, 16, 16, 16);
-        pStrip_a->set_pixel(pStrip_a, 3, 16, 16, 16);
-        pStrip_a->set_pixel(pStrip_a, 4, 16, 16, 16);
+        pStrip_a->set_pixel(pStrip_a, 3, 255, 1255, 0);
+        pStrip_a->set_pixel(pStrip_a, 4, 255, 0, 0);
 
         /* Refresh the strip to send data */
         pStrip_a->refresh(pStrip_a, 100);
@@ -49,28 +48,10 @@ static void configure_led(void)
 {
     ESP_LOGI(TAG, "Example configured to blink addressable LED!");
     /* LED strip initialization with the GPIO and pixels number*/
-    pStrip_a = led_strip_init(CONFIG_BLINK_LED_RMT_CHANNEL, BLINK_GPIO, 5);
+    pStrip_a = led_strip_init(0, BLINK_GPIO, 5);
     /* Set all LED off to clear all pixels */
     pStrip_a->clear(pStrip_a, 50);
 }
-
-#elif CONFIG_BLINK_LED_GPIO
-
-static void blink_led(void)
-{
-    /* Set the GPIO level according to the state (LOW or HIGH)*/
-    gpio_set_level(BLINK_GPIO, s_led_state);
-}
-
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-    gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-}
-
-#endif
 
 void app_main(void)
 {
