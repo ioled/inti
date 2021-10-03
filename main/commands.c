e5d54e5ac8fd3eb63bbbc034343519d48885e0d1
tesslaray <cristian.valdivia@alumnos.usm.cl>
2021-10-03 10:26:38 -0300

    feature: added event ok from iot core

--- main/commands.c
+++ main/commands.c
@@ -1,7 +1,7 @@
 #include "ota.c"
 
 /*  Executes a routine depending on the type of command received 
-    by the device from the Iot Core.  */
+    by the device from the IoT Core.  */
 void apply_command(char* command_from_iot_core)
 {   
     int compare_string_with_reset_result;
@@ -9,6 +9,9 @@ void apply_command(char* command_from_iot_core)
 
     int compare_string_with_ota_result;
     compare_string_with_ota_result = strcmp(command_from_iot_core,  "\"ota\"");
+
+    int compate_string_with_ok_result;
+    compare_string_with_ok_result = strcmp(command_from_iot_core,  "\"ok\"");
 
     if (compare_string_with_reset_result == 0) {
         ESP_LOGI(TAG, "Restarting now ...");
@@ -20,6 +23,10 @@ void apply_command(char* command_from_iot_core)
         ESP_LOGI(TAG, "Update !");    
         turn_strip_led_color(THANOS);
         xTaskCreate(&simple_ota_example_task, "ota_example_task", 8192, NULL, 5, NULL);
-    
     } 
+
+    if (compate_string_with_ok_result == 0) {
+        ESP_LOGI(TAG, "Operation its Ok !");        
+    }
+
 }
